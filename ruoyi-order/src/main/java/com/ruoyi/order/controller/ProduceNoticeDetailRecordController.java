4ad6fbe01fe4769c8703575cd79aa9a01e0fe5f0
be1ongs <whoisbe1ongs@gmail.com>
2024-02-29 18:49:14 +0800

    生产通知单

--- ruoyi-order/src/main/java/com/ruoyi/order/controller/ProduceNoticeDetailRecordController.java
+++ ruoyi-order/src/main/java/com/ruoyi/order/controller/ProduceNoticeDetailRecordController.java
@@ -5,11 +5,7 @@ import org.apache.shiro.authz.annotation.RequiresPermissions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 import com.ruoyi.common.annotation.Log;
 import com.ruoyi.common.enums.BusinessType;
 import com.ruoyi.order.domain.ProduceNoticeDetailRecord;
@@ -73,8 +69,8 @@ public class ProduceNoticeDetailRecordController extends BaseController
     /**
      * 新增生产通知单明细记录
      */
-    @GetMapping("/add/{produceNoticeDetailId}")
-    public String add(@PathVariable("produceNoticeDetailId") Long produceNoticeDetailId)
+    @GetMapping("/add")
+    public String add(@RequestParam("produceNoticeDetailId") Long produceNoticeDetailId)
     {
         return prefix + "/add";
     }
